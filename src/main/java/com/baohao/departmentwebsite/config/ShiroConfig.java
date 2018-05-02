94287e92ad549497fa7fedb149c2a05d1cc04ebf
ZhangFengyi <fengyi@fit2cloud.com>
2018-05-02 22:28:24 +0800

    fix login bug

--- src/main/java/com/baohao/departmentwebsite/config/ShiroConfig.java
+++ src/main/java/com/baohao/departmentwebsite/config/ShiroConfig.java
@@ -1,12 +1,18 @@
 package com.baohao.departmentwebsite.config;
 
+import com.baohao.departmentwebsite.realm.ShiroDBRealm;
 import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 import java.util.Map;
 
@@ -36,6 +42,7 @@ public class ShiroConfig {
     public DefaultWebSecurityManager securityManager() {
         DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
         defaultWebSecurityManager.setSessionManager(sessionManager());
+//        defaultWebSecurityManager.setRealm(shiroDBRealm());
         return defaultWebSecurityManager;
     }
 
@@ -52,4 +59,45 @@ public class ShiroConfig {
         webSessionManager.setSessionIdCookie(cookie);
         return webSessionManager;
     }
+
+    @Bean(name = "shiroDBRealm")
+    @DependsOn("lifecycleBeanPostProcessor")
+    public ShiroDBRealm shiroDBRealm() {
+        return new ShiroDBRealm();
+    }
+
+    @Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+
+//    @Bean(name = "shiroDBRealm")
+//    @DependsOn("lifecycleBeanPostProcessor")
+//    public ShiroDBRealm getShiroDBRealm() {
+//        return new ShiroDBRealm();
+//    }
+//
+//    @Bean(name = "lifecycleBeanPostProcessor")
+//    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+//        return new LifecycleBeanPostProcessor();
+//    }
+
+//    等到 ApplicationConText 加载完成之后再装配 shiroRealm
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        ApplicationContext context = event.getApplicationContext();
+        ((DefaultWebSecurityManager) context.getBean("securityManager")).setRealm(shiroDBRealm());
+    }
+
+
+    /**
+     * 等到ApplicationContext 加载完成之后 装配shiroRealm
+     */
+//    @EventListener
+//    public void handleContextRefresh(ContextRefreshedEvent event) {
+//        ApplicationContext context = event.getApplicationContext();
+//
+//        ((DefaultWebSecurityManager) context.getBean("securityManager")).setRealm(getShiroDBRealm());
+//    }
 }
